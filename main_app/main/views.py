c9bc69830ddcd8038663482be1fcf47712f44b7d
EugGol <dragonborn1613@gmail.com>
2025-09-07 12:10:34 +0300

    create views for main and include settings for app

--- main_app/main/views.py
+++ main_app/main/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
 # Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'description': 'Home description',
+        'bool': True
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse("About page")
+
+
